755fd8049e78528f6dcb214bdc388a4372be1b68
Panchis7u7 <smadrigal.rod@gmail.com>
2020-05-23 00:03:18 -0500

    Pruebas..

--- Prueba/Prueba.asm
+++ Prueba/Prueba.asm
@@ -1,15 +1,21 @@
 section .data
-
+	var1 db 255,40,255
+	var2 db 65
 section .bss
 	prueba resq 1
 section .text
 global main
+
 main:
 	push rbp
 	mov rbp, rsp
-	mov rcx, 0xa
-	mov [rcx], prueba
-	mov [prueba], rcx 
+	
+	xor rbx, rbx
+	xor rcx, rcx
+	lea rbx, [var1]
+	mov rcx, [rbx+1]
+	mov cl, [var2]
+	
 	mov rsp, rbp ;leave
 	pop rbp
 ret
